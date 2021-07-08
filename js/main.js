6e6ab07b24afe38a57327380745aaf3ba74246ce
g0 <59220768+g0-I@users.noreply.github.com>
2021-07-08 23:52:31 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -33,7 +33,7 @@ var app4 = new Vue({
 var app5 = new Vue({
   el: '#app-5',
   data: {
-    message: 'Hello Vue.js!'
+    message: 'Hello Vue.js'
   },
   methods: {
     reverseMessage: function () {
